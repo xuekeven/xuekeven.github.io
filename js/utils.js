63b12320f973122f0d69ded82edb7b0f410f094f
xuekeven <kevenchn@qq.com>
2025-07-18 15:21:21 +0800

    Site updated: 2025-07-18 15:21:21

--- js/utils.js
+++ js/utils.js
@@ -15,6 +15,16 @@ Fluid.utils = {
     window.removeEventListener('scroll', callback);
   },
 
+  listenDOMLoaded(callback) {
+    if (document.readyState !== 'loading') {
+      callback();
+    } else {
+      document.addEventListener('DOMContentLoaded', function () {
+        callback();
+      });
+    }
+  },
+
   scrollToElement: function(target, offset) {
     var of = jQuery(target).offset();
     if (of) {
@@ -25,83 +35,91 @@ Fluid.utils = {
     }
   },
 
-  elementInViewport: function(element, heightFactor) {
-    heightFactor = heightFactor || 1;
+  elementVisible: function(element, offsetFactor) {
+    offsetFactor = offsetFactor && offsetFactor >= 0 ? offsetFactor : 0;
     var rect = element.getBoundingClientRect();
-    var height = window.innerHeight || document.documentElement.clientHeight;
-    var top = rect.top;
-    return (top >= 0 && top <= height * (heightFactor + 1))
-      || (top <= 0 && top >= -(height * heightFactor) - rect.height);
+    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+    return (
+      (rect.top >= 0 && rect.top <= viewportHeight * (1 + offsetFactor) + rect.height / 2) ||
+      (rect.bottom >= 0 && rect.bottom <= viewportHeight * (1 + offsetFactor) + rect.height / 2)
+    );
   },
 
-  waitElementVisible: function(selectorsOrElement, callback, heightFactor) {
+  waitElementVisible: function(selectorOrElement, callback, offsetFactor) {
     var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
-        && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-    var supportsIntersectionObserver = 'IntersectionObserver' in window;
-
+    var isBot = (runningOnBrowser && !('onscroll' in window))
+      || (typeof navigator !== 'undefined' && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
     if (!runningOnBrowser || isBot) {
-      callback();
       return;
     }
 
-    var target;
-    if (typeof selectorsOrElement === 'string') {
-      target = document.querySelector(selectorsOrElement);
-    } else {
-      target = selectorsOrElement;
-    }
+    offsetFactor = offsetFactor && offsetFactor >= 0 ? offsetFactor : 0;
 
-    var _heightFactor = heightFactor || 2;
-
-    if (Fluid.utils.elementInViewport(target, _heightFactor)) {
-      callback();
-      return;
-    }
-
-    if (supportsIntersectionObserver) {
-      var io = new IntersectionObserver(function(entries, ob) {
-        if (entries[0].isIntersecting) {
+    function waitInViewport(element) {
+      Fluid.utils.listenDOMLoaded(function() {
+        if (Fluid.utils.elementVisible(element, offsetFactor)) {
           callback();
-          ob.disconnect();
+          return;
         }
-      }, {
-        threshold : [0],
-        rootMargin: (window.innerHeight || document.documentElement.clientHeight) + 'px'
-      });
-      io.observe(target);
-    } else {
-      var warpCallback = Fluid.utils.listenScroll(function() {
-        if (Fluid.utils.elementInViewport(target, _heightFactor)) {
-          Fluid.utils.unlistenScroll(warpCallback);
-          callback();
+        if ('IntersectionObserver' in window) {
+          var io = new IntersectionObserver(function(entries, ob) {
+            if (entries[0].isIntersecting) {
+              callback();
+              ob.disconnect();
+            }
+          }, {
+            threshold : [0],
+            rootMargin: (window.innerHeight || document.documentElement.clientHeight) * offsetFactor + 'px'
+          });
+          io.observe(element);
+        } else {
+          var wrapper = Fluid.utils.listenScroll(function() {
+            if (Fluid.utils.elementVisible(element, offsetFactor)) {
+              Fluid.utils.unlistenScroll(wrapper);
+              callback();
+            }
+          });
         }
       });
+    }
+
+    if (typeof selectorOrElement === 'string') {
+      this.waitElementLoaded(selectorOrElement, function(element) {
+        waitInViewport(element);
+      });
+    } else {
+      waitInViewport(selectorOrElement);
     }
   },
 
-  waitElementLoaded: function(targetId, callback) {
+  waitElementLoaded: function(selector, callback) {
     var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
-    && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-
+    var isBot = (runningOnBrowser && !('onscroll' in window))
+      || (typeof navigator !== 'undefined' && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
     if (!runningOnBrowser || isBot) {
-      callback();
       return;
     }
 
     if ('MutationObserver' in window) {
       var mo = new MutationObserver(function(records, ob) {
-        var ele = document.getElementById(targetId);
+        var ele = document.querySelector(selector);
         if (ele) {
-          callback();
+          callback(ele);
           ob.disconnect();
         }
       });
       mo.observe(document, { childList: true, subtree: true });
     } else {
-      document.addEventListener('DOMContentLoaded', function() {
-        callback();
+      Fluid.utils.listenDOMLoaded(function() {
+        var waitLoop = function() {
+          var ele = document.querySelector(selector);
+          if (ele) {
+            callback(ele);
+          } else {
+            setTimeout(waitLoop, 100);
+          }
+        };
+        waitLoop();
       });
     }
   },
@@ -125,10 +143,9 @@ Fluid.utils = {
         s.onload = onload;
       }
     }
-    var e = document.getElementsByTagName('script')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
-    e.parentNode.insertBefore(s, e);
+    var ss = document.getElementsByTagName('script');
+    var e = ss.length > 0 ? ss[ss.length - 1] : document.head || document.documentElement;
+    e.parentNode.insertBefore(s, e.nextSibling);
   },
 
   createCssLink: function(url) {
@@ -137,22 +154,51 @@ Fluid.utils = {
     l.setAttribute('type', 'text/css');
     l.setAttribute('href', url);
     var e = document.getElementsByTagName('link')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
+      || document.getElementsByTagName('head')[0]
+      || document.head || document.documentElement;
     e.parentNode.insertBefore(l, e);
   },
 
-  loadComments: function(selectors, loadFunc) {
+  loadComments: function(selector, loadFunc) {
     var ele = document.querySelector('#comments[lazyload]');
     if (ele) {
       var callback = function() {
         loadFunc();
         ele.removeAttribute('lazyload');
       };
-      Fluid.utils.waitElementVisible(selectors, callback, CONFIG.lazyload.offset_factor);
+      Fluid.utils.waitElementVisible(selector, callback, CONFIG.lazyload.offset_factor);
     } else {
       loadFunc();
     }
+  },
+
+  getBackgroundLightness(selectorOrElement) {
+    var ele = selectorOrElement;
+    if (typeof selectorOrElement === 'string') {
+      ele = document.querySelector(selectorOrElement);
+    }
+    var view = ele.ownerDocument.defaultView;
+    if (!view) {
+      view = window;
+    }
+    var rgbArr = view.getComputedStyle(ele).backgroundColor.replace(/rgba*\(/, '').replace(')', '').split(/,\s*/);
+    if (rgbArr.length < 3) {
+      return 0;
+    }
+    var colorCast = (0.213 * rgbArr[0]) + (0.715 * rgbArr[1]) + (0.072 * rgbArr[2]);
+    return colorCast === 0 || colorCast > 255 / 2 ? 1 : -1;
+  },
+
+  retry(handler, interval, times) {
+    if (times <= 0) {
+      return;
+    }
+    var next = function() {
+      if (--times >= 0 && !handler()) {
+        setTimeout(next, interval);
+      }
+    };
+    setTimeout(next, interval);
   }
 
 };
@@ -166,6 +212,7 @@ function Debouncer(callback) {
   this.callback = callback;
   this.ticking = false;
 }
+
 Debouncer.prototype = {
   constructor: Debouncer,
 
